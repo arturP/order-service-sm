ebe26f1597f9863e48e7c88f793d4d53fd1ffbaa
Artur Przychodzen <artur2p@gmail.com>
2021-11-04 12:22:14 +0100

    initial

---
+++ src/main/java/io/artur/spring/orderservicesm/OrderServiceSmApplication.java
@@ -0,0 +1,13 @@
+package io.artur.spring.orderservicesm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceSmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceSmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/artur/spring/orderservicesm/OrderServiceSmApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artur.spring.orderservicesm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceSmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
